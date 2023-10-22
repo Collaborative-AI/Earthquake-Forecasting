f9405241ed08e6fa82e95fe16b937969ef1563cd
Luke Nam <lsn12@duke.edu>
2023-10-22 09:31:00 -0400

    canada data modified

---
+++ src/merge/north_america/preprocess.py
@@ -0,0 +1,87 @@
+# import helper functions for data preprocessing
+import sys
+sys.path.append("src/merge")
+from helper import replace_with_timestamp, remove_unknown_magnitudes, remove_unknown_coordinates
+
+# import pandas for data manipulation
+import pandas as pd
+from csv import writer
+
+def preprocess_Canada():
+    csv_file = "src/merge/north_america/raw/Canada-19850109-20230621.csv"
+
+    # read the csv and only keep the specified columns
+    df = pd.read_csv(csv_file)
+    df = df[["Date", "Time", "Magnitude", "Latitude", "Longitude", "Depth/km"]]
+    df.rename(columns={"Depth/km": "Depth"}, inplace=True)
+    
+    # reformat times, and remove unknown magnitudes + coordinates
+    df = replace_with_timestamp(df)
+    df = remove_unknown_magnitudes(df)
+    df = remove_unknown_coordinates(df)
+
+    # drop duplicates from the dataframe
+    df = df.drop_duplicates()
+
+    # store the result in a CSV
+    df.to_csv("src/merge/noaa/input/Canada-19850109-20230621.csv", index=False)
+
+
+def preprocess_NCEI():
+    csv_file = "src/merge/noaa/raw/NCEI.csv"
+
+    # read the csv and only keep the specified columns
+    df = pd.read_csv(csv_file)
+    df = df[["Year", "Mo", "Dy", "Hr", "Mn", "Sec", "Mag", "Latitude", "Longitude", "Focal Depth (km)"]]
+    df.rename(columns={"Mo": "Month",
+                             "Dy": "Day",
+                             "Hr": "Hour",
+                             "Mn": "Minute",
+                             "Sec": "Second",
+                             "Mag": "Magnitude",
+                             "Focal Depth (km)": "Depth"}, inplace=True)
+    
+    # reformat times, and remove unknown magnitudes + coordinates
+    df = replace_with_timestamp(df)
+    df = remove_unknown_magnitudes(df)
+    df = remove_unknown_coordinates(df)
+
+    # drop duplicates from the dataframe
+    df = df.drop_duplicates()
+
+    # store the result in a CSV
+    df.to_csv("src/merge/noaa/input/NCEI.csv", index=False)
+
+
+def preprocess_NOAA():
+    csv_file = "src/merge/noaa/raw/NOAA.csv";
+
+    # read the csv and only keep the specified columns
+    df = pd.read_csv(csv_file)
+    df = df[["YEAR", "MONTH", "DAY", "HOUR", "MINUTE", "SECOND", "MAGNITUDE", "LATITUDE", "LONGITUDE", "EQ_DEPTH"]]
+    df.rename(columns={"YEAR": "Year",
+                             "MONTH": "Month",
+                             "DAY": "Day",
+                             "HOUR": "Hour",
+                             "MINUTE": "Minute",
+                             "SECOND": "Second",
+                             "MAGNITUDE": "Magnitude",
+                             "LATITUDE": "Latitude",
+                             "LONGITUDE": "Longitude",
+                             "EQ_DEPTH": "Depth"}, inplace=True)
+    
+    # reformat times, and remove unknown magnitudes + coordinates
+    df = replace_with_timestamp(df)
+    df = remove_unknown_magnitudes(df)
+    df = remove_unknown_coordinates(df)
+
+    # drop duplicates from the dataframe
+    df = df.drop_duplicates()
+
+    # store the result in a CSV
+    df.to_csv("src/merge/noaa/input/NOAA.csv", index=False)
+
+# rune the pre-processing functions
+preprocess_GHEA()
+preprocess_NCEI()
+preprocess_NOAA()
--- src/scrapers/Canada/CanadaScraper.py
+++ src/scrapers/Canada/CanadaScraper.py
@@ -9,14 +9,34 @@ def find_quakes(input_path: str, output_path: str):
 
             # label the header of the csv with the appropriate labels
             csv_writer = writer(out_file, lineterminator="\n")
+            header = ["Year", "Month", "Day", "Hour", "Minute", "Second",
+                      "Time", "Latitude", "Longitude", "Depth/km",
+                      "MagType", "Magnitude", "EventLocationName"]
+            csv_writer.writerow(header)
+            next(input_file, None)
 
             # write each row from the txt file to the csv
             for line in input_file:
                 words = line.split("|")
-                csv_writer.writerow(words)
+
+                # extract the date and time information from the rows
+                date, time = words[1].split("T")
+
+                # extract the year, month, and day
+                year, month, day = date.split("-")
+                year, month, day = int(year), int(month), int(day)
+
+                # extract the hour, minute, and second
+                time = time[:-5]
+                hour, minute, second = time.split(":")
+                hour, minute, second = int(hour), int(minute), int(second)
+
+                # update the row with the new date and time columns
+                datetime = [year, month, day, hour, minute, second]
+                csv_writer.writerow(datetime + words[2:])
 
 # main method that calls the web scraper function
 if __name__ == "__main__":
-    input_path = "Canada/Canada-19850109-20230621.txt"
-    output_path = "Canada/Canada-19850109-20230621.csv"
+    input_path = "src/scrapers/Canada/Canada-19850109-20230621.txt"
+    output_path = "src/scrapers/Canada/Canada-19850109-20230621.csv"
     find_quakes(input_path, output_path)
