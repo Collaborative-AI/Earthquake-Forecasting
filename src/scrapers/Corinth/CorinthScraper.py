cb36098dbe148a297a093deb15dd22f0413e6e9b
lukelike1001 <lsn12@duke.edu>
2023-10-27 13:58:25 -0400

    rescraped corinth

--- src/scrapers/Corinth/CorinthScraper.py
+++ src/scrapers/Corinth/CorinthScraper.py
@@ -9,21 +9,33 @@ def find_quakes(input_path: str, output_path: str):
 
             # label the header of the csv with the appropriate labels
             csv_writer = writer(out_file, lineterminator="\n")
-            header = ["Year", "Origin Time", "Latitude", "Longitude", "Depth",
-                      "Magnitude", "Decimal Years", "Time Relative to First Earthquake",
-                      "Event ID", "Cluster ID (sorted by #events)",
-                      "Cluster ID (by time)", "Multiplet ID", "#events in Multiplet",
-                      "E-W horizontal error", "N-S horizontal error",
-                      "Vertical error"]
+            header = ["Year", "Month", "Day", "Hour", "Minute", "Second",
+                      "Magnitude", "Latitude", "Longitude", "Depth"]
             csv_writer.writerow(header)
 
             # write each row from the txt file to the csv
             for line in input_file:
                 words = line.split()
-                csv_writer.writerow(words)
+
+                # find the separate values from the time
+                year = words[0]
+                time = words[1]
+                if "." in time: time = time[:time.index(".")]
+                stamps = [time[max(0, k-2):k] for k in range(len(time), -1, -2)]
+                second, minute, hour, day, month = stamps[:5]
+
+                # find the remaining values
+                mag = words[5]
+                lat = words[2]
+                lon = words[3]
+                dep = words[4]
+
+                # write the row into the result csv
+                row = [year, month, day, hour, minute, second, mag, lat, lon, dep]
+                csv_writer.writerow(row)
 
 # main method that calls the web scraper function
 if __name__ == "__main__":
-    input_path = "Corinth/Marathias_seq.txt"
-    output_path = "Corinth/Corinth Gulf 2020-21 Seismic Crisis.csv"
+    input_path = "src/scrapers/Corinth/Marathias_seq.txt"
+    output_path = "src/scrapers/Corinth/Corinth Gulf 2020-21 Seismic Crisis.csv"
     find_quakes(input_path, output_path)
