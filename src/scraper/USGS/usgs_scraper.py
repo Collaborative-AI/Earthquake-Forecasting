9e01a289817cec29cde179e67ae2e1a1a8c5fcd4
Young-Hoo Jun <yj112@duke.edu>
2024-01-05 15:55:16 -0500

    [Refactor] updated usgs_scraper.py to prevent bot detection

--- src/scraper/USGS/usgs_scraper.py
+++ src/scraper/USGS/usgs_scraper.py
@@ -2,23 +2,32 @@ import pandas as pd
 import requests
 import io
 from datetime import datetime, timedelta
+import random
+import time
 
+# Download USGS Data
+# Takes 12 hours to run
 def download_data(start_date, end_date):
-    print("Hello World 1")
+    # USGS request format 
     base_url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
 
     data = pd.DataFrame()  
+
+    #limit time frame to scraper
     while start_date < end_date:
+        # each request consists of 30days of data 
         next_date = start_date + timedelta(days=30)
         if next_date > end_date:
             next_date = end_date
-        
+
+        # parameters used to create the correct request url
         parameters = {
             "format": "csv",
             "starttime": start_date.strftime("%Y-%m-%d"),
             "endtime": next_date.strftime("%Y-%m-%d")
         }
 
+        # make request using the created url
         response = requests.get(base_url, params=parameters)
         response.raise_for_status() 
 
@@ -26,6 +35,11 @@ def download_data(start_date, end_date):
         data = pd.concat([data, csv_data])
         
         start_date = next_date
+
+        # Sleep for a random interval between 3 to 5 minutes
+        time_to_sleep = random.randint(180, 300)  # Random time between 180 and 300 seconds
+        print(f"Sleeping for {time_to_sleep} seconds")
+        time.sleep(time_to_sleep)
 
     return data
 
@@ -37,4 +51,4 @@ end_date = datetime(2023, 6, 12)
 earthquake_data = download_data(start_date, end_date)
 
 # Save to CSV
-earthquake_data.to_csv("earthquake_data.csv", index=False)
+earthquake_data.to_csv("/mnt/data/earthquake_data.csv", index=False)
