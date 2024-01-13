38aa5913624eade5328e2bd2ed471931c0c5d63c
lukelike1001 <lsn12@duke.edu>
2024-01-12 20:17:34 -0500

    texas updated

--- src/scraper/Texas/TexasScraper.py
+++ src/scraper/Texas/TexasScraper.py
@@ -1,5 +1,11 @@
+# import helper functions for data cleaning
+import sys
+sys.path.append("src/scraper")
+from helper import clean_data
+
 # run on python 3.11.2
 from csv import writer
+import pandas as pd
 
 # converts a txt file (separated by whitespace) to a csv file
 def find_quakes(input_path: str, output_path: str):
@@ -9,40 +15,55 @@ def find_quakes(input_path: str, output_path: str):
 
             # label the header of the csv with the appropriate labels
             csv_writer = writer(out_file, lineterminator="\n")
-            header = ["Year", "Month", "Day", "Hour", "Minute", "Second",
+            header = ["Year", "Month", "Day", "Hour", "Minute", "Second", "Millisecond",
                       "Magnitude", "Latitude", "Longitude", "Depth"]
             csv_writer.writerow(header)
             next(input_file, None)
 
             # write each row from the txt file to the csv
             for line in input_file:
-                words = line.split(",")
+                row = line.split(",")
 
                 try:
-                    # put each time unit (excluding ms) in separate columns
-                    year, month, day = words[2].split("-")
-                    hour, minute, second = words[3].split(":")
-
-                    # convert them into integers
-                    year, month, day = int(year), int(month), int(day)
-                    hour, minute, second = int(hour), int(minute), int(second)
+                    
+                    # convert the date and time into a pd.Timestamp
+                    datetime = f"{row[2]} {row[3]}"
+                    ts = pd.Timestamp(datetime)
+                    
+                    # extract time information from the timestamp
+                    year = ts.year
+                    month = ts.month
+                    day = ts.day
+                    hour = ts.hour
+                    minute = ts.minute
+                    second = ts.second
+                    millisecond = ts.microsecond // 1000
 
                     # find the other rows with data
-                    magnitude = words[4]
-                    latitude = words[6]
-                    longitude = words[8]
-                    depth = words[11]
+                    magnitude = row[4]
+                    latitude = row[6]
+                    longitude = row[8]
+                    
+                    # note: MSL = mean sea level
+                    # there are depth values relative to ground surface and MSL,
+                    # and here, we choose MSL for consistency with other datasets
+                    depth = row[10]
 
                     # reformat the line
-                    data = [year, month, day, hour, minute, second,
-                             magnitude, latitude, longitude, depth]
-                    csv_writer.writerow(data)
+                    output_row = [year, month, day, hour, minute, second, millisecond,
+                                  magnitude, latitude, longitude, depth]
+                    csv_writer.writerow(output_row)
                 
                 except Exception as e:
                     print(str(e))
 
+
 # main method that calls the web scraper function
 if __name__ == "__main__":
-    input_path = "src/scraper/Texas/texnet_events.csv"
-    output_path = "src/scraper/Texas/Texas Earthquakes (2016-2023).csv"
+    input_path = "src/scraper/Texas/raw/texnet_events.csv"
+    
+    output_filename = "Texas (2016-2023)"
+    output_path = f"src/scraper/Texas/clean/{output_filename}.csv"
+    
     find_quakes(input_path, output_path)
+    clean_data(output_path)
