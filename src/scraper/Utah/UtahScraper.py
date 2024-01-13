4a063dd937c441b410cf09ad738faecb38ec3458
lukelike1001 <lsn12@duke.edu>
2024-01-12 21:10:58 -0500

    utah updated

--- src/scraper/Utah/UtahScraper.py
+++ src/scraper/Utah/UtahScraper.py
@@ -1,3 +1,8 @@
+# import helper functions for data cleaning
+import sys
+sys.path.append("src/scraper")
+from helper import clean_data
+
 # run on python 3.11.2
 from csv import writer
 
@@ -9,29 +14,39 @@ def find_quakes(input_path: str, output_path: str):
 
             # label the header of the csv with the appropriate labels
             csv_writer = writer(out_file, lineterminator="\n")
-            header = ["Year", "Month", "Day", "Hour", "Minute", "Second", "Latitude", "Longitude",
-                      "Depth", "Template Event Magnitude", "Detection Magnitude",
-                      "Event Template ID", "Detection ID", "Correlation Coefficient"]
+            header = ["Year", "Month", "Day", "Hour", "Minute", "Second", "Millisecond",
+                      "Magnitude", "Latitude", "Longitude", "Depth"]
             csv_writer.writerow(header)
 
             # write each row from the txt file to the csv
             for line in input_file:
-                words = line.split()
+                row = line.split()
 
                 # find the date by splitting it every two chars
-                date = words[1]
-                month, day, hour, minute, second = [date[2*k:2*k+2] for k in range(5)]
-
+                year = int(row[0])
+                date = row[1]
+                month, day, hour, minute, second = [int(date[2*k:2*k+2]) for k in range(5)]
+                millisecond = int(float(date[-3:]) * 1000)
+                
+                # extract the geographical information
+                magnitude = row[6]
+                latitude = row[2]
+                longitude = row[3]
+                depth = row[4]
+                
                 # cast from strings to ints
-                year, month, day = int(words[0]), int(month), int(day)
-                hour, minute, second = int(hour), int(minute), int(second)
+                output_row = [year, month, day, hour, minute, second, millisecond,
+                                  magnitude, latitude, longitude, depth]
+                csv_writer.writerow(output_row)
 
-                # update the row's values
-                words = [year, month, day, hour, minute, second] + words[2:]
-                csv_writer.writerow(words)
 
 # main method that calls the web scraper function
 if __name__ == "__main__":
-    input_path = "src/scraper/Utah/detections.txt"
-    output_path = "src/scraper/Utah/Mineral Mountains, Utah 2016-19.csv"
+    input_path = "src/scraper/Utah/raw/detections.txt"
+    
+    output_filename = "Utah (2016-2019)"
+    output_path = f"src/scraper/Utah/clean/{output_filename}.csv"
+    
     find_quakes(input_path, output_path)
+    clean_data(output_path)
+    
