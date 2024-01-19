d900020383e3ec19a7818de67161dd12334c9ac7
lukelike1001 <lsn12@duke.edu>
2024-01-19 15:11:25 -0500

    merge first draft

--- src/merge/helper.py
+++ src/merge/helper.py
@@ -1,19 +1,20 @@
 # run on python 3.11.2
 import pandas as pd
 
+# define a constant for time comparisons
+base = pd.Timestamp(year=1, month=1, day=1, hour=0, minute=0, second=0, tz="UTC")
+
 """
 INPUT:  Row with Schema ["Timestamp", "Magnitude", "Latitude", "Longitude", "Depth"]
 OUTPUT: Rounded Timestamp (to 10 seconds), and Magnitude, Latitude and Longitude
         to 1 decimal place
 """
 def round_row(input_row):
-    timestamp = input_row["Timestamp"]
 
     # round the seconds to the nearest tens
     # here, we can do some arithmetic magic by instead dividing the total seconds by 10
     # all earthquakes happening within a 10-second window will share the same rtime value
     ts = pd.Timestamp(input_row["Timestamp"])
-    base = pd.Timestamp(year=1, month=1, day=1, hour=0, minute=0, second=0)
     timedelta = ts - base
     rtime = int(timedelta.total_seconds() / 10)
 
--- src/merge/merge.py
+++ src/merge/merge.py
@@ -21,21 +21,30 @@ resulting CSV is guaranteed to be:
 """
 
 """
-Merge North America Batch
-Includes the following earthquakes:
-- Canada
-- NCEDC
-- New Madrid
-- Pacific Northwest (PNW)
-- Southern California (SoCal)
-- Texas
-- Utah (Mineral Mountains)
+STEP 1:
+Find all the filepaths to all datasets unrelated to USGS and SAGE.
+Modify the SCRAPER_PATH if your working directory isn't the root.
 """
 
-csv_files = ["Canada/clean/Canada (1985-2024).csv",
-             "NCEDC/clean/NCEDC (1984-2023).csv",
-             "New Madrid/clean/New Madrid (1974-2023).csv",
+SCRAPER_PATH = "src/scraper/"
+csv_files = ["Argentina/clean/Argentina Andean Earthquakes (2016-2017).csv",
+             "Canada/clean/Canada (1985-2024).csv",
+             "East Africa/clean/East Africa Rift System (1994-2022).csv",
+             "GHEA/clean/GHEA (1000-1903).csv",
+             "Intensity/clean/U.S. Earthquake Intensity Database (1638-1985).csv",
+             "NOAA/clean/NOAA NCEI-WDS (0-2023).csv",
              "Pacific Northwest/clean/PNW Tremors (2009-2023).csv",
              "SoCal/clean/SCEDC (1932-2023).csv",
+             "South Asia/clean/South Asia (1900-2014).csv",
              "Texas/clean/Texas (2016-2023).csv",
-             "Utah/clean/Utah (2016-2019).csv"]
+             "Turkey/clean/Turkey (1915-2021).csv",
+             "World Tremor/clean/World Tremor Database (2005-2014).csv"]
+
+csv_files = [SCRAPER_PATH + csv_file for csv_file in csv_files]
+
+"""
+STEP 2:
+Define the output path and run the merge function.
+"""
+output_path = "src/merge/Various-Catalogs.csv"
+merge_group(csv_files, output_path)
