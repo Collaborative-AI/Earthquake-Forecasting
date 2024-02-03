59edc35d5919e2f968db2a1e3952dcd3e0fab55d
lukelike1001 <lsn12@duke.edu>
2024-02-02 21:31:16 -0500

    modularize file changes

---
+++ src/scraper/SoCal/__init__.py
@@ -0,0 +1,2 @@
+from .SoCal_File_modularize import *
+from .SoCal_Web_modularize import *
---
+++ src/scraper/__init__.py
@@ -0,0 +1,11 @@
+import sys
+from .Ancient import *
+from .Argentina import *
+from .Canada import *
+# from .Corinth import *
+from .GHEA import *
+# from .New_Madrid import *
+from .SoCal import *
+# from .SRCMOD import *
+from .USGS import *
+# from .Utah import *
